3777ec6da7510307927a8df95e58a4f9a8ec4285
Carson Johnson <carson74johnson@gmail.com>
2021-11-01 22:27:20 -0400

    added to the dashboard.js to handle the bew blogpost handler

---
+++ public/js/dashboard.js
@@ -0,0 +1,46 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+
+    const name = document.querySelector('#blogpost-name').value.trim();
+    const description = document.querySelector('#blogpost-desc').value.trim();
+
+    if (name && description) {
+        const responst = await fetch('/api/blogposts', {
+            method: 'POST',
+            body: JSON.stringify({ name, description }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (Response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to create tech post')
+        }
+    }
+};
+
+const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+        const id = event.target.getAttribute('data-id');
+
+        const response = await fetch(`/api/blogposts/${id}`, {
+            method: 'DELETE',
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to delete blogpost');
+        }
+    }
+};
+
+document
+    .querySelector('.new-blogpost-form')
+    .addEventListener('submit', newFormHandler);
+
+document
+    .querySelector('.blogpost-list')
+    .addEventListener('click', delButtonHandler);
