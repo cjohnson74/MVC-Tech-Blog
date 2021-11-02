c01abc0ef9c61083423680941448036bc1db12c8
Carson Johnson <carson74johnson@gmail.com>
2021-11-01 22:07:19 -0400

    added to seed.js

---
+++ seeds/seed.js
@@ -0,0 +1,25 @@
+const sequelize = require('../config/connection');
+const { User, Blogpost } = require('../models');
+
+const userData = require('./userData.json');
+const blogpostData = require('./blogpostData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+
+    const users = await User.bultCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    for (const blogpost of blogpostData) {
+        await Blogpost.create({
+            ...blogpost,
+            user_id: users[Math.floor(Math.random() * users.length)].id,
+        });
+    }
+
+    process.exit(0);
+};
+
+seedDatabase();
