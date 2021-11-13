186f39fd20dc9797131d22092fbc52b770ba5540
Carson Johnson <carson74johnson@gmail.com>
2021-11-12 22:56:33 -0500

    fixed associations in index.js in models folder

--- models/index.js
+++ models/index.js
@@ -2,31 +2,23 @@ const User = require('./User');
 const Blogpost = require('./Blogpost');
 const Comment = require('./Comment');
 
+Blogpost.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
 User.hasMany(Blogpost, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE',
 });
 
-User.hasMany(Comment, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE',
+Blogpost.belongsToMany(Comment, {
+    through: BlogpostComment,
+    foreignKey: 'blogpost_id'
 });
 
-Blogpost.hasMany(Comment, {
-    foreignKey: 'blogpost_id',
-    onDelete: 'CASCADE'
-});
-
-Blogpost.belongsTo(User, {
-    foreignKey: 'user_id',
-});
-
-Comment.belongsTo(User, {
-    foreignKey: 'user_id',
-});
-
-Comment.belongsTo(Blogpost, {
-    foreignKey: 'blogpost_id',
+Comment.belongsToMany(Blogpost, {
+    through: BlogpostComment,
+    foreignKey: 'comment_id'
 });
 
 module.exports = { User, Blogpost, Comment };
